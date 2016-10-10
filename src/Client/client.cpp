ecb926181e994fe4f81ec94e99398b8d8af03ddb
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2016-10-10 14:17:07 -0600

    Refactored the client implementation into a class

---
+++ src/Client/client.cpp
@@ -0,0 +1,192 @@
+// STL
+#include <cassert>
+#include <string.h>
+
+// Boost
+#include <boost/array.hpp>
+
+// Project
+#include "client.h"
+
+using boost::asio::ip::udp;
+
+const uint16_t updateInterval = 100;
+
+// #TODO_AH fill in implementation notes
+
+//------------------------------------------------------------------ constructor
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+client::client(
+	boost::asio::io_service& ioService) :
+	m_resolver(ioService),
+	m_UDPsocket(ioService)
+{
+	this->m_terminate = false;
+
+	this->m_activeProtocol =
+		client::protocol::UDP;
+
+	const std::string host = "localhost";
+	const std::string port = "8080";
+
+	udp::resolver::query serverQuery(
+		udp::v4(),
+		host,
+		port);
+
+	this->m_serverEndPoint =
+		*this->m_resolver.resolve(serverQuery);
+
+	this->m_UDPsocket.open(
+		udp::v4());
+
+	// necessary? getting a crash later when trying to receive if removed
+	boost::array<char, 1> send_buf = {{0}};
+	m_UDPsocket.send_to(
+		boost::asio::buffer(send_buf), m_serverEndPoint);
+};
+
+//-------------------------------------------------------------------------- run
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::run()
+{
+	// thread for input/sending messages
+	m_threads.create_thread(
+		boost::bind(&client::inputLoop, this));
+
+	// thread for receiving messages
+	m_threads.create_thread(
+		boost::bind(&client::receiveLoop, this));
+
+	m_threads.join_all();
+};
+
+//-------------------------------------------------------------------- inputLoop
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::inputLoop()
+{
+	while(!this->m_terminate)
+	{
+		std::string message("");
+
+		// communication with the server
+		std::cout << "Enter a message: " << std::endl;
+		std::cin >> message;
+
+		if(message == "/exit")
+		{
+			this->m_terminate = true;
+			break;
+		}
+		else
+		{
+			// Branch on protocol
+			switch(this->m_activeProtocol)
+			{
+				case client::protocol::UDP:
+				{
+					this->sendOverUDP(message);
+					break;
+				}
+				case client::protocol::Bluetooth:
+				{
+					this->sendOverBluetooth(message);
+					break;
+				}
+				case client::protocol::Undefined:
+				default:
+				{
+					assert(false);
+					break;
+				}
+			}
+		}
+	}
+};
+
+//------------------------------------------------------------------ sendOverUDP
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::sendOverUDP(
+	const std::string& message)
+{
+	std::cout << "test" << std::endl;
+	m_UDPsocket.send_to(
+		boost::asio::buffer(message, message.size()),
+		m_serverEndPoint);
+};
+
+//------------------------------------------------------------ sendOverBluetooth
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::sendOverBluetooth(
+	const std::string& message)
+{
+	// #TODO implement sending over Bluetooth for the client
+};
+
+//------------------------------------------------------------------ receiveLoop
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::receiveLoop()
+{
+	while(!this->m_terminate)
+	{
+		this->receiveOverUDP();
+		this->receiveOverBluetooth();
+	}
+};
+
+//--------------------------------------------------------------- receiveOverUDP
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::receiveOverUDP()
+{
+	try
+	{
+		// Listen for any data the server endpoint sends back
+		boost::array<char, 128> recv_buf; // make this a member variable? how do we determine size?
+
+		size_t incomingMessageLength =
+			m_UDPsocket.receive_from(
+				boost::asio::buffer(recv_buf), 
+				this->m_serverEndPoint);
+
+		if(incomingMessageLength > 0)
+		{
+			// output data
+			std::cout.write(
+				recv_buf.data(),
+				incomingMessageLength);
+		}
+
+		std::cout << std::endl;
+
+		// sleep
+		boost::this_thread::sleep(
+			boost::posix_time::millisec(updateInterval));
+	}
+	catch(std::exception& exception)
+	{
+		std::cout << exception.what() << std::endl;
+	}
+};
+
+//--------------------------------------------------------- receiveOverBluetooth
+// Implementation notes:
+//  None
+//------------------------------------------------------------------------------
+void client::receiveOverBluetooth()
+{
+	// #TODO implement receiving over Bluetooth for the client
+};
---
+++ src/Client/client.h
@@ -0,0 +1,45 @@
+// Boost
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+
+class string;
+
+class client
+{
+	enum class protocol
+	{
+		Undefined,
+		UDP,
+		Bluetooth
+	};
+
+public:
+	// #TODO_AH header comment blocks
+	client(
+		boost::asio::io_service &ioService);
+
+	void run();
+
+private:
+	void inputLoop();
+
+	void sendOverUDP(
+		const std::string& message);
+
+	void sendOverBluetooth(
+		const std::string& message);
+
+	void receiveLoop();
+
+	void receiveOverUDP();
+
+	void receiveOverBluetooth();
+
+	// Member Variables
+	boost::asio::ip::udp::socket m_UDPsocket;
+	boost::asio::ip::udp::resolver m_resolver;
+	boost::asio::ip::udp::endpoint m_serverEndPoint;
+	boost::thread_group m_threads;
+	client::protocol m_activeProtocol;
+	bool m_terminate;
+};
