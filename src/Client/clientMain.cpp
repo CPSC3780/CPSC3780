4f5b75d5b74bbcfff92e511124817cac742ecd03
TigleyM <tigleym@gmail.com>
2016-11-08 13:58:33 -0700

    Derp what am I thinking

--- src/Client/clientMain.cpp
+++ src/Client/clientMain.cpp
@@ -13,7 +13,7 @@ int main()
 	{
 		std::string username("");
 		std::cout << "Enter your username: " << std::endl;
-		std::cin >> username;
+		std::getline(std::cin, username);
 		boost::asio::io_service ioService;
 
 		client clientInstance(username, ioService);
