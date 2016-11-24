dc8d859e230267aa0539cb3f142efd0f14c70b98
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2016-11-23 22:40:30 -0700

    - Increase update interval

--- src/Common/constants.h
+++ src/Common/constants.h
@@ -10,7 +10,7 @@
 namespace constants
 {
 	const uint16_t updateIntervalMilliseconds = 50;
-	const uint16_t syncIntervalMilliseconds = 200;
+	const uint16_t syncIntervalMilliseconds = 1500;
 
 	const std::vector<uint16_t> serverListeningPorts(
 	{8080, 8081, 8082, 8083, 8084});
